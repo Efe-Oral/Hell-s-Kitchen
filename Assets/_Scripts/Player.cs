651911ac0c889f4741a7116736604c0ae2918137
efeoral <efeoral@gmail.com>
2026-03-27 00:17:57 +0100

    Enhance player movement logic with hugging the wall movement

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -28,12 +28,49 @@ public class Player : MonoBehaviour
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = 0.6f;
         float playerHeight = 2f;
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + new Vector3(0f, playerHeight, 0f), playerRadius, moveDir, moveDistance);
+        Debug.DrawLine(transform.position + new Vector3(0f, 1f, 0f), transform.position + moveDir + new Vector3(0f, 1f, 0f), Color.green);
+
+        if (!canMove)
+        {
+            // Attemp moving only on X-axis
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + new Vector3(0f, playerHeight, 0f), playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+
+            else
+            {
+                // Cannot move on the X-axis, Let's try to move in Z-axis
+
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z); ;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + new Vector3(0f, playerHeight, 0f), playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+
+                else
+                {
+                    // We can't move anywhere, we are stuck
+                }
+
+            }
+
+        }
+
 
         if (canMove)
         {
             gameObject.transform.position = transform.position + moveDir * moveDistance;
         }
+
+
         gameObject.transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
